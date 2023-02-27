5646b704db4f670046375d7d6894cd65babbaed0
hjhyun98 <110337559+hjhyun98@users.noreply.github.com>
2023-02-27 23:02:57 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.03 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -0,0 +1,11 @@
+def solution(my_string):
+    answer = 0
+    temp = 0
+    for i in my_string:
+        if i.isdigit():
+            temp = temp*10+int(i)
+        else:
+            answer += temp
+            temp = 0
+    answer += temp
+    return answer
